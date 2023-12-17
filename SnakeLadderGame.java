aa27ced187eddb35f48a55cdd98a7633174ca06b
sriramadithya001 <143174882+sriramadithya001@users.noreply.github.com>
2023-12-17 13:14:32 +0530

    Update SnakeLadderGame.java

--- SnakeLadderGame.java
+++ SnakeLadderGame.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 import java.util.Queue;
 import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Random;
 
@@ -15,12 +16,40 @@ class Player {
     }
 }
 
+class Snake {
+    static int id;
+    int snake_id;
+    int head;
+    int tail;
+
+    Snake(int head,int tail) {
+        this.snake_id = ++id;
+        this.head = head;
+        this.tail = tail;
+    }
+}
+
+class Ladder {
+    static int id;
+    int ladder_id;
+    int down;
+    int up;
+
+    Ladder(int down,int up) {
+        this.ladder_id = ++id;
+        this.down = down;
+        this.up = up;
+    }
+}
+
 public class SnakeLadderGame {
 
     static int[][] board = new int[10][10];
     static Queue<Player> players = new LinkedList<>();
-    static HashMap<Integer,Integer> snakes = new HashMap<>();
-    static HashMap<Integer,Integer> ladders = new HashMap<>();
+    static ArrayList<Snake> snakes = new ArrayList<>();
+    static ArrayList<Ladder> ladders = new ArrayList<>();
+    static HashMap<Integer,Integer> snake_pos = new HashMap<>();
+    static HashMap<Integer,Integer> ladder_pos = new HashMap<>();
     static Random rand = new Random();
 
     public static void addPlayers() {
@@ -28,20 +57,22 @@ public class SnakeLadderGame {
     }
 
     public static void addSnakes() {
-        for(int i=0;i<10;) {
+        for(int i=0;i<5;) {
             int head = rand.nextInt(100) + 1 , tail = rand.nextInt(100) + 1;
-            if(head > tail && head != 100 && snakes.containsKey(head) == false && snakes.containsValue(head) == false) {
-                snakes.put(head,tail);
+            if(head > tail && head != 100 && snake_pos.containsKey(head) == false && snake_pos.containsValue(head) == false) {
+                snakes.add(new Snake(head,tail));
+                snake_pos.put(head,tail);
                 i++;
             }
         }
     }
 
     public static void addLadders() {
-        for(int i=0;i<10;) {
+        for(int i=0;i<5;) {
             int down = rand.nextInt(100) + 1 , up = rand.nextInt(100) + 1;
-            if(down < up && up != 100 && ladders.containsKey(down) == false && snakes.containsKey(down) == false) {
-                ladders.put(down,up);
+            if(down < up && up != 100 && ladder_pos.containsKey(down) == false && snake_pos.containsKey(down) == false) {
+                ladders.add(new Ladder(down,up));
+                ladder_pos.put(down,up);
                 i++;
             }
         }
@@ -60,13 +91,13 @@ public class SnakeLadderGame {
 
             int new_pos = player.pos + rollDice();
 
-            if(snakes.containsKey(new_pos)) {
+            if(snake_pos.containsKey(new_pos)) {
                 System.out.println("Sanke bite at " + new_pos);
-                new_pos = snakes.get(new_pos);
+                new_pos = snake_pos.get(new_pos);
             }
-            else if(ladders.containsKey(new_pos)) {
+            else if(ladder_pos.containsKey(new_pos)) {
                 System.out.println("Climbing ladder at " + new_pos);
-                new_pos = ladders.get(new_pos);
+                new_pos = ladder_pos.get(new_pos);
             }
 
             if(new_pos <= 100) {
@@ -97,10 +128,34 @@ public class SnakeLadderGame {
         return flag;
     }
 
+    public static boolean printSnake(int val) {
+        for(Snake s : snakes) {
+            if(s.head == val || s.tail == val) {
+                System.out.print("[s" + s.snake_id + "]");
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean printLadder(int val) {
+        for(Ladder l : ladders) {
+            if(l.down == val || l.up == val) {
+                System.out.print("[l" + l.ladder_id + "]");
+                return true;
+            }
+        }
+        return false;
+    }
+
     public static void printBoard() {
         for(int i=0;i<10;i++) {
             for(int j=0;j<10;j++) {
-                if(!printPlayers(board[i][j])) System.out.print(board[i][j] + " ");
+                if(printPlayers(board[i][j])) continue;
+                if(printSnake(board[i][j])) continue;
+                if(printLadder(board[i][j])) continue;
+                System.out.print(board[i][j] + " ");
+
             }
             System.out.println("\n");
         }
